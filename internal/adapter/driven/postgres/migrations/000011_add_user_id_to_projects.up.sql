ab3b37b8b8f3bef75b784e3cd094471f04422a08
Saúl Antonio Morquecho Cela <samc920130@hotmail.com>
2026-04-10 20:29:31 -0600

    feat: truncate projects table before adding mandatory user_id column

--- internal/adapter/driven/postgres/migrations/000011_add_user_id_to_projects.up.sql
+++ internal/adapter/driven/postgres/migrations/000011_add_user_id_to_projects.up.sql
@@ -1,2 +1,5 @@
+-- Remove orphaned projects that predate user ownership.
+TRUNCATE projects CASCADE;
+
 ALTER TABLE projects
     ADD COLUMN user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE;
